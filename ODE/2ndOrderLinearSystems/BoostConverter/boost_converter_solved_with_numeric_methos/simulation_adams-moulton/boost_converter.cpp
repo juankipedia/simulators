5f2438601c33fe0e44445e01bfe2cd7787d6a98f
JuanDiego <juandiegp17@gmail.com>
2019-06-22 22:41:37 -0400

    adding

--- ODE/2ndOrderLinearSystems/BoostConverter/boost_converter_solved_with_numeric_methos/simulation_adams-moulton/boost_converter.cpp
+++ ODE/2ndOrderLinearSystems/BoostConverter/boost_converter_solved_with_numeric_methos/simulation_adams-moulton/boost_converter.cpp
@@ -6,9 +6,10 @@
 		dv/dt = - (5000/9)v +  ((i)U/ 0.00002)
 
 	system is going to be aproximated on:
+		
 		0 <= t <= 0.1
-	
-	with initial conditions w0, w1, z0 and z1.
+
+	divided in 2000 equal sub intervals, with initial conditions w0, w1, z0 and z1.
  */
 
 
@@ -18,10 +19,11 @@ using namespace std;
 # define endl "\n"
 typedef unsigned long long int ulli;
 
-ulli n; 
+
 long double w_, z_, w, z, t;
 const long double U = 1;
-const long double h = 0.05;
+const long double h = 5e-5;
+const ulli n = 2000; 
 
 long double f(long double i, long double v){
 	return -200 * i - (U * v/ 0.05) + 2000 * U;
@@ -32,12 +34,18 @@ long double g(long double i, long double v){
 }
 
 long double w_plus_one(long double wi, long double zi, long double wi_, long double zi_ ){
-
-	return 0;
+	long double teta = f(wi, zi)/30000 - 5e-5*f(wi_, zi_)/12;
+	long double phi = g(wi, zi)/30000 - 5e-5*g(wi_, zi_)/12;
+	return(
+		252760800*wi/253922420 - 432*zi/1053620 - 432*phi/1053620 + 0.041476053 +  252760800*teta/253922420
+	);
 }
 
 long double z_plus_one(long double wi, long double zi, long double wi_, long double zi_, long double wplus1){
-	return 0;
+	long double phi = g(wi, zi)/30000 - 5e-5*g(wi_, zi_)/12;
+	return (
+		432*zi/437 + 450*wplus1/437 + 432*phi/437
+	);
 }
 
 
@@ -45,9 +53,7 @@ void read_input(){
 	cout << endl;
 
 	cout << "Linear Differential System Equations will be approximated on the interval [" 
-	<< 0 << ", " << 0.1 << "]." << endl <<
-	"Now in order to use Adams-Moulton Method please insert N (to divide interval in N equally spaced numbers) " << endl;
-	cin >> n;
+	<< 0 << ", " << 0.1 << "]. this interval is going to be devided in 2000 sub intervals" << endl;
 	cout << endl;
 	
 	cout << "Please Insert Initial condition for the Current w0" << endl;
